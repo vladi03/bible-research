c89b76c6e73388dbe25fa5a823b2e541b826b590
Caleb <martinez.caleb56@gmail.com>
2025-10-03 21:03:18 -0400

    init functions commit

--- bible-research-api/functions/research-functions/researchRoute.js
+++ bible-research-api/functions/research-functions/researchRoute.js
@@ -1,5 +1,6 @@
 import {cors, isFirebaseAuth} from "./lib/utils/routeUtils.js";
 import {onRequest} from "firebase-functions/https";
+import {placeholderService} from "./services/placeholderVerse.js";
 
 export const test = onRequest({timeoutSeconds: 540},
     async (request, response) => {
@@ -14,5 +15,19 @@ export const test = onRequest({timeoutSeconds: 540},
         }
     });
 });
+export const verse = onRequest({timeoutSeconds: 540},
+    async (request, response) => {
+        await cors(request, response, async () => {
+            const { book, pageNumber } = request.body;
+            try {
 
-export default { test };
+                response.send(await placeholderService());
+            } catch (error) {
+                console.error('Error sending email:', error);
+                response.status(500).send({ error: 'Failed to send email' });
+            }
+        });
+    });
+
+
+export default { test ,verse};
